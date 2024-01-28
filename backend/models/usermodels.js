ccee16da043468f552a0a33bf9c441aac448e8ad
Martin <ilhammartin2@gmail.com>
2024-01-28 23:29:46 +0700

    set schema

---
+++ backend/models/usermodels.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const Admin = mongoose.Schema(
+    {
+        username: {
+            type:String,
+            required:true
+        },
+        password: {
+            type:String,
+            required:true
+        }
+    }
+    )
